9cc61786d771b139e31f053ed2e336451ad2c5d0
olona <oddbjorn_lona@hotmail.com>
2022-05-23 21:26:26 +0200

    Tutorial 6: fix

--- core/src/main/kotlin/axehigh/SilverBullet.kt
+++ core/src/main/kotlin/axehigh/SilverBullet.kt
@@ -6,7 +6,6 @@ import com.badlogic.gdx.Application.LOG_DEBUG
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.g2d.Batch
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import jdk.internal.org.jline.utils.Log
 import ktx.app.KtxGame
 import ktx.log.Logger
 import ktx.log.logger
@@ -20,14 +19,14 @@ class SilverBullet : KtxGame<SilverBulletScreen>() {
 
     override fun create() {
         Gdx.app.logLevel = LOG_DEBUG
-        LOG.debug { "Silverbullet starts"] }
+        LOG.debug { "Silverbullet starts" }
         addScreen(GameScreen(this, batch))
         setScreen<GameScreen>()
     }
 
     override fun dispose() {
         super.dispose()
-        Log.debug{"Sprites in batch: ${(batch as SpriteBatch).maxSpritesInBatch}"}
+        LOG.debug{"Sprites in batch: ${(batch as SpriteBatch).maxSpritesInBatch}"}
         batch.dispose()
     }
 
